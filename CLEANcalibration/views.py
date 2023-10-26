2358c4a91a82d2a83c8112ec087f7b9d39015d38
Leonardo Hoinaski <leonardo.hoinaski@ufsc.br>
2023-10-26 15:39:59 -0300

    updating

--- CLEANcalibration/views.py
+++ CLEANcalibration/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from .forms import CLEANcalibrationNewDevice_form
+from .models import CLEANcalibrationNewDevice
 from django.contrib import messages
 
+
 from django.shortcuts import render, get_object_or_404, redirect
-
-
 
 
 
@@ -13,8 +13,40 @@ def CLEANcalibrationNewDevice (request):
         form = CLEANcalibrationNewDevice_form(request.POST, request.FILES)
         if form.is_valid():
         	form.save()
+            
+            CLEANcalib = CLEANcalibrationNewDevice.objects.all().last()
+            BASE = (os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+            deviceId = CLEANcalib.deviceId
+
+            RUN_FOLDER = BASE + "/media/calibration/"+ str(deviceId) + "/"
+            if os.path.isdir(BASE + "/media/getElev/") ==0:
+                os.mkdir(BASE + "/media/getElev/")
+            if os.path.isdir(RUN_FOLDER) ==1:
+                shutil.rmtree(RUN_FOLDER)
+                os.mkdir(RUN_FOLDER)
+            else:
+                os.mkdir(RUN_FOLDER)
+
+            STATIC_SRTM = BASE+'/static/inventory/SRTM/'
+            SRTM_selectGetElev(STATIC_SRTM,RUN_FOLDER,getElev_OBJ.srcFile.path)
+            getElevSRTM(RUN_FOLDER,getElev_OBJ.srcFile.path)
+
+            file_path = RUN_FOLDER + "/getElev.csv"
+            if os.path.exists(file_path):
+                with open(file_path, 'rb') as fh:
+                    response = HttpResponse(fh.read(), content_type="text/csv")
+                    response['Content-Disposition'] = 'attachment; filename=' + os.path.basename(file_path)
+                    return response
+                    #return redirect('blog-smart')
+            shutil.rmtree(RUN_FOLDER)
+            shutil.rmtree(BASE + "/media/getElev/")
+
+
         	messages.success(request, f'Your file was updated!')
+
         	return redirect('CLEANcalibrationNewDevice')
+
+
             
     else:
         form = CLEANcalibrationNewDevice_form()
